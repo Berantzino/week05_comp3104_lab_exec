6c96b4b5ef495e32918c1338cf1868873297c111
Berantzino <lasse@berantzino.dk>
2020-10-08 01:54:49 +0200

    adds all files

---
+++ build/precache-manifest.99d0354a03d0cd366a9c98282c4e2e4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cc8604ba4345317c836",
+    "url": "/static/css/main.d84490b1.chunk.css"
+  },
+  {
+    "revision": "7cc8604ba4345317c836",
+    "url": "/static/js/main.7cc8604b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "17cc18bbd2585cf059051032c28a79a2",
+    "url": "/index.html"
+  }
+];
